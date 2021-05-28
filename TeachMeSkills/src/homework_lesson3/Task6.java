983e777af2c508ccaca2f8b5efa724a707acbb5d
Dmitry <Dmitry@DESKTOP-3KI6QOB>
2021-05-29 01:25:45 +0300

    Task#6

---
+++ TeachMeSkills/src/homework_lesson3/Task6.java
@@ -0,0 +1,33 @@
+package homework_lesson3;
+
+import java.util.Random;
+
+public class Task6 {
+public static void main(String[] args) {
+ 
+Random r = new Random();
+int [] mass = new int[4];
+	for(int i = 0; i<mass.length; i++) {
+		mass[i] = r.nextInt(11);
+	}
+System.out.println("Array:");
+	for(int i = 0; i<mass.length; i++) {
+	System.out.print(mass[i] + " ");
+	}
+System.out.println();
+boolean prove = false;
+	for(int i = 0; i<mass.length-1; i++) {
+		if (mass[i]>mass[i+1]) {
+			System.out.println("Array is NOT strictly ascending sequence");
+			prove = true;
+			break;
+		} 
+				
+	}
+	
+	if (prove == false) {
+		System.out.println("Array IS strictly ascending sequence");
+	}
+
+}
+}
