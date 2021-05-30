d0cd6760bdea42b8139f63af336e23028ad6242b
Dmitry <Dmitry@DESKTOP-3KI6QOB>
2021-05-30 15:53:34 +0300

    Task #11

---
+++ TeachMeSkills/src/homework_lesson3/Task11.java
@@ -0,0 +1,34 @@
+package homework_lesson3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task11 {
+public static void main(String[] args) {
+//Creation of the array with n length and filling it with random num-s from 0 to 9
+Scanner sc = new Scanner(System.in);
+Random r = new Random();
+System.out.print("Enter the lenght of the array: ");
+int a = sc.nextInt();
+System.out.print("Generated array: ");
+int [] mass = new int[a];
+	for(int i = 0; i <mass.length; i++) {
+		mass[i] = r.nextInt(10);
+		System.out.print(mass[i] + " ");
+	}
+System.out.println();
+//
+System.out.println("IMPORTANT INFORMATION");
+System.out.println("In this particular case we won't recognize zero as a num that can divided by three");
+int product = 1;
+System.out.print("Nums that can divided by 3 are: ");
+for (int i = 0; i < mass.length; i++) {
+	if((mass[i] % 3 == 0) && (mass[i] != 0)){
+		product *=mass[i];
+		System.out.print(mass[i] + " ");
+	}
+}
+System.out.println();
+System.out.println("Product of num-s that can divided by 3 is: " + product);
+}
+}
