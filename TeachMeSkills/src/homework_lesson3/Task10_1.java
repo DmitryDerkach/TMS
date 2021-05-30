a7cbb02e8cff355e167a7a259fa750e75fdb6187
Dmitry <Dmitry@DESKTOP-3KI6QOB>
2021-05-30 12:48:56 +0300

    Minor fixes

---
+++ TeachMeSkills/src/homework_lesson3/Task10_1.java
@@ -0,0 +1,63 @@
+package homework_lesson3;
+//Решение задачи №10 от Димы Поверова
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task10_1 {
+public static void main(String[] args) {
+
+	  int n;
+      int evenCount = 0;
+
+      Scanner sc = new Scanner(System.in);
+      Random rand = new Random();
+
+      System.out.print("Enter a number more than 3: ");
+
+      n = sc.nextInt();
+
+      while (n<4) {
+          System.out.println("You've entered a wrong number");
+          System.out.print("Reenter it: ");
+          n = sc.nextInt();
+      }
+
+      int[] arr1 = new int[n];
+
+      for (int i=0; i<arr1.length; i++) {
+          arr1[i] = rand.nextInt(n);       //Заполняем массив рандомными числами
+          System.out.print(arr1[i] + " "); 
+          if (arr1[i]%2 == 0 && arr1[i]!=0) {		//Определяем кол-во четных чисел
+              evenCount++;
+          }
+      }
+
+      System.out.println("\nIn the array even " + ((evenCount>1)? "numbers are " : "number is ") + evenCount); //использование тернарной операции
+
+      int[] arr2 = new int [evenCount];
+
+      for (int i=0, j=0; i<arr1.length; i++) {
+          if (arr1[i]%2 == 0 && arr1[i]!=0) {
+              arr2[j] = arr1[i];
+              j++;
+          }
+      }
+
+      for (int x: arr2) {
+          System.out.print(x + " ");
+      }
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
