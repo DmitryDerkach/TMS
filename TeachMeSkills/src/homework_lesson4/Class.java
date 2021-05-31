f697fe72fc24855a55d1a87d9d6b6484e5984749
Dmitry <Dmitry@DESKTOP-3KI6QOB>
2021-05-31 14:53:38 +0300

    Class

---
+++ TeachMeSkills/src/homework_lesson4/Class.java
@@ -0,0 +1,17 @@
+package homework_lesson4;
+
+public class Class {
+public static void main(String[] args) {
+	
+/*N-dimensional array 
+ * int [][] arr = new int [][] --- [] - number of arrays. Ex: 
+ * int [][] arr4 = {{1,2},{2,4},{5,7}}*/
+
+	
+	
+	
+	
+	
+	
+}
+}
