c28a2403f6b251c686441b80b3baa13ee0fdd1d6
Dmitry <Dmitry@DESKTOP-3KI6QOB>
2021-06-02 13:38:45 +0300

    First commit

---
+++ TeachMeSkills/src/homework_lesson5/Class.java
@@ -0,0 +1,10 @@
+package homework_lesson5;
+//В этом пэкэджэ должны быть задания с листочка, начинающегося фразой Класс Phone
+public class Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
