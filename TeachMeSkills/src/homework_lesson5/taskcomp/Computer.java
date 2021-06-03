c63cd672ef29c2586ac038f56261ea4a9f329d3a
Dmitry <Dmitry@DESKTOP-3KI6QOB>
2021-06-03 11:32:29 +0300

    Some changes

--- TeachMeSkills/src/homework_lesson5/taskcomp/Computer.java
+++ TeachMeSkills/src/homework_lesson5/taskcomp/Computer.java
@@ -3,61 +3,85 @@ package homework_lesson5.taskcomp;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Computer {
-
-String Core;
- int RAM;
- int HDD;
- int resourse;
- boolean turn = true;
+public class Computer {									//Вынес в отдельный класс         
+														//Название класса с большой буквы 
+String сore;
+ protected int ram;												//Наименование полей класса с маленькой буквы 
+ protected int hdd;
+ protected int resourse;
+ 
+ private boolean live = true;
+ private boolean boot = false; 
+ //private int liveresourse = 5;
 	 
  Computer(String a, int b, int c){
-	 Core = a;
-	 RAM = b;
-	 HDD = c;
+	 сore = a;
+	 ram = b;
+	 hdd = c;
+ }
+ 
+ Computer(){
+	 
  }
  
  void displayInfo() {
-	 System.out.printf("Проверка внесенных данных: \nCore: %s \nRAM: %d \nHDD: %d \n", Core, RAM, HDD);
+	 
+	 System.out.printf("Проверка внесенных данных: \nCore: %s \nRAM: %d \nHDD: %d \n", сore, ram, hdd);
  }
   
  void turnOn(){
+	 boot = true;
 	 System.out.println("Попытка включения компьютера. Введите число 0 или 1");
-	 if (turn == false) {
+	 if (live == false) {								//Добавил оператор ветвления для проверки, сгорел компьютер или нет. Ненужный break убрал
 		 System.out.println("Ваш компьютер больше не запустится. Он сгорел");
-//			 break;  На этом моменте хочу прервать выполнение инструкций метода, но компилятор ругается
-	 }
+	 } else {
 	 Scanner sc = new Scanner(System.in);
 	 int a = sc.nextInt();
 	 Random r = new Random();
 	 int b = r.nextInt(2);
 	 if (b == a) {
-	 System.out.println("Компьютер включен. Хотите выключить его? Введите 1, если Да и 0, если нет");
-	 int c = sc.nextInt();
-	 if (c > 0) {			
-//			 void turnOff;			//Как мне из одного метода перепрыгнуть в другой метод?
-	 }
-	 } else { System.out.println("Ваш компьютер сгорел. Ваш компьютер больше не запустится");
-	 }
-	 turn = false; 		//Как мне после заврешения работы какого-то метода вернуться к началу его выполнения? Т.е. как мне его зациклить?
-//В данном случае, я хочу, чтобы после окончания метода turnOn он увидел в консоли сначала строку 42, а потом 44
+		 System.out.println("Компьютер включен.Ресурс компьютера снижен на 1 Хотите выключить его? Введите 1, если Да и 0, если нет");
+		 int c = sc.nextInt();
+		 	if (c > 0) {			
+		 		turnOff();			// Убрал void, поставил скобки
+		 	} else {
+		 		System.out.println("Компьютер находится в ждущем режиме.");
+		 	}
+	 	} else {
+		 System.out.println("Ваш компьютер сгорел. Ваш компьютер больше не запустится");
+		 live = false; 
+		 }
+		 do {
+			 turnOn();
+		 } while (live = true);		//Добавил цикл do-while
+	 	
+	 }             
  }
 	  
  void turnOff(){
-	 if (turn == false) {
-		 System.out.println("Ваш компьютер не запустится");
+	 if (boot == false) {
+		 System.out.println("Вы не можете выключить выключенный компьютер");
+	 } else {
+	 if (live == false) {
+		 System.out.println("Ваш компьютер не запустится.");
 	 }
 	 System.out.println("Попытка выключения компьютера. Введите число от 0 или 1");
 	 Scanner sc = new Scanner(System.in);
 	 int a = sc.nextInt();
-	 sc.close();
 	 Random r = new Random();
 	 int b = r.nextInt(2);
 	 if (b == a) {
-	 System.out.println("Компьютер выключен");
-	 } else { System.out.println("Ваш компьютер сгорел. Ваш компьютер больше не запустится"); 
-	 }
-	 turn = false; 
-	 }
-		
-}
+		 System.out.println("Компьютер вылючен. Хотите включить его? Введите 1, если Да и 0, если нет");
+		 int c = sc.nextInt();
+		 	if (c > 0) {			
+		 		turnOn();			
+		 	} else {
+		 		System.out.println("Компьютер выключен");
+		 	}
+	 	} else {
+		 System.out.println("Ваш компьютер сгорел. Ваш компьютер больше не запустится");
+		 live = false;
+	 		}
+	 	}	
+	 }             
+} //Скобка класса Computer
