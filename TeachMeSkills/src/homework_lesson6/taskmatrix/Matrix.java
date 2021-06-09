bba9cdd402ab5145831510cd577b79ac307f0e06
Dmitry <Dmitry@DESKTOP-3KI6QOB>
2021-06-09 15:52:44 +0300

    Что я делаю, одному богу известно

--- TeachMeSkills/src/homework_lesson6/taskmatrix/Matrix.java
+++ TeachMeSkills/src/homework_lesson6/taskmatrix/Matrix.java
@@ -4,9 +4,9 @@ public class Matrix {
 	/*Поля*/
 	private int column;
 	private int line;
-	private int [][] arr;
-	private boolean trigger = false;
-	private int supnum = 1;
+	private static int [][][] arrsup; /*Трехмерный массив, в который мы будем складывать двухмерные массивы для последующих вычислений*/
+	private boolean trigger = false; /*Триггер для проверок*/
+	private static int supnum = 1;/*Вспомогательное число, которым будем крутить ячейки трехмерного массива*/
 	/*Методы*/
 	public int getColumn() {
 		return column;
@@ -20,52 +20,75 @@ public class Matrix {
 	public void setLine(int line) {
 		this.line = line;
 	}
+	/*Создаем матрицу*/
 	public void createMatrix() {
 		trigger = true;
-		arr = new int [line][column];
+		arrsup = new int[100][][]; /*Создаем трехмерный массив, который будет хранить наш двумерный массив*/
+		arrsup [supnum-1] = new int [line][column]; /*Создаем двумерный массив ячейки трехмерного массива размерностью line * column*/
 		/*Заполняем матрицу рандомными значениями*/	
 			Random r = new Random();
 			for (int i = 0; i < line; i++) {
 				for (int j = 0; j < column; j++) {
-					arr [i][j] = r.nextInt(11);
+					arrsup[supnum-1][i][j] = r.nextInt(11);
 				}
 			}
-		/*Выводим исходную матрицу на экран*/
-		System.out.println("Матрица №" + supnum + ":"); /*Почему при создании второй матрицы 34 строчка все равно вы-
-		выодит "Матрица №1:", хотя я добавил иттерацию переменной supnum?*/
+//		/*Выводим исходную матрицу на экран*/
+		System.out.println("Матрица №" + supnum + ":"); 
 		for (int i = 0; i < line; i++) {
 			for (int j = 0; j < column; j++) {
-				System.out.printf(" %3d", arr [i][j]);
+				System.out.printf(" %3d", arrsup[supnum-1] [i][j]);
 			}
 			System.out.println();
 		}
-		supnum += supnum; /*Добавлена иттерация*/
+		supnum += supnum; 
 	}
-	/*Умножение матрицы на число*/
-	public void multiplicationBy(int num) {
-		if (trigger == false) {
-			System.out.println("Вы не можете пользоваться операцией умножения, пока не сгенерируете матрицу!");
-			return;
+	/*Метод, который будет выводить на экран ту матрицу, которая нам была нужна*/
+	public void displayMatrix(int n) {
+		System.out.println("Выводим на экран матрицу под номером " + n + ":");
+		for (int i = 0; i < line; i++) {
+			for (int j = 0; j < column; j++) {
+				System.out.printf(" %3d", arrsup[n-1][i][j]);
+			}
+			System.out.println();
 		}
-	/*Умножаем элементы матрицы на число num*/	
-			for (int i = 0; i < line; i++) {
-				for (int j = 0; j < column; j++) {
-					arr[i][j] = num * arr[i][j];
-				}
-			}
-	/*Выводим полученную матрицу на экран*/		
-			System.out.println("Матрица, умноженная на число " + num + ":");
-			for (int i = 0; i < line; i++) {
-				for (int j = 0; j < column; j++) {
-					System.out.printf(" %3d", arr [i][j]);
-				}
-				System.out.println();
-			}
-	/*Сложение матрицы с дпругой матрицей*/		
-//	public void matrixAddition() {
-//		
-//	}
-			
 	}
 	
+//	/*Умножение матрицы на число*/
+//	public void multiplicationBy(int num) {
+//		if (trigger == false) {
+//			System.out.println("Вы не можете пользоваться операцией умножения, пока не сгенерируете матрицу!");
+//			return;
+//		}
+//	/*Умножаем элементы матрицы на число num*/	
+//			for (int i = 0; i < line; i++) {
+//				for (int j = 0; j < column; j++) {
+//					arrsup[supnum] = 
+//				}
+//			}
+//	/*Выводим полученную матрицу на экран*/		
+//			System.out.println("Матрица, умноженная на число " + num + ":");
+//			for (int i = 0; i < line; i++) {
+//				for (int j = 0; j < column; j++) {
+//					System.out.printf(" %3d", arrsup[supnum-1] [i][j]);
+//				}
+//				System.out.println();
+//			}
+//	}
+	/*Сложение матрицы с другой матрицей*/		
+//	public void matrixplusmatrix(Matrix a, Matrix b) {
+//		for (int i = 0; i < line; i++) {
+//			for (int j = 0; j< column; j++) {
+//				int [][] arrresult = new int [line][column];
+//				arrresult[i][j] = a[i][j] + b[i][j];
+//				
+//				
+//			}
+//		}
+		
 }
+
+	
+	
+	
+	
+
--- TeachMeSkills/src/homework_lesson6/taskmatrix/MatrixApplication.java
+++ TeachMeSkills/src/homework_lesson6/taskmatrix/MatrixApplication.java
@@ -5,18 +5,19 @@ public static void main(String[] args) {
 	
 Matrix test = new Matrix();
 /*Создаем матрицу 3*3*/
-test.setColumn(4);
+test.setColumn(3);
 test.setLine(3);
 /*Генерируем нашу матрицу*/
 test.createMatrix();
+test.createMatrix();
 /*Вызываем метод умножения на число*/	
-//test.multiplicationBy(2);	
+//test.multiplicationBy(2);
+//Matrix test1 = new Matrix();
+//test1.setColumn(3);
+//test1.setLine(3);
+//test1.createMatrix();	
+test.displayMatrix(1);
 
-Matrix test1 = new Matrix();
-test1.setColumn(4);
-test1.setLine(3);
-test1.createMatrix();	
-	
 	
 	
 	
