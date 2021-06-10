5419585e74736cca93658866f014fad4febaaddd
Dmitry <Dmitry@DESKTOP-3KI6QOB>
2021-06-10 15:51:41 +0300

    Fix

--- TeachMeSkills/src/homework_lesson7/taskstudent/StudentApplication.java
+++ TeachMeSkills/src/homework_lesson7/taskstudent/StudentApplication.java
@@ -2,11 +2,6 @@ package homework_lesson7.taskstudent;
 
 public class StudentApplication {
 public static void main(String[] args) {
-	
-    Student student1 = new Aspirant("Ivan", "Ivanov", 105, 5, "Programming");
-    Student student2 = new Student("Vasia", "Vasiliev", 108, 5);
-    Student student3 = new Aspirant("Kostia", "Ivanov", 204, 5, "Engineering");
-	
     Student [] arr = new Student[3];
 		arr [0] = new Aspirant("Ivan", "Ivanov", 105, 5, "Programming");
 		arr [1] = new Student("Vasia", "Vasiliev", 108, 5);
