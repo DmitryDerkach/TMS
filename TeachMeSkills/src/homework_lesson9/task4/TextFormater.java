841f7187837295973a9e31b0d5d3f4d6c95d1e18
Dmitry <Dmitry@DESKTOP-3KI6QOB>
2021-06-16 15:13:57 +0300

    Commit

--- TeachMeSkills/src/homework_lesson9/task4/TextFormater.java
+++ TeachMeSkills/src/homework_lesson9/task4/TextFormater.java
@@ -1,9 +1,12 @@
 package homework_lesson9.task4;
 
 public class TextFormater {
+	
+private String sentence;
 
-	public static void redactor(String sentence) {
-		
+	public void redactor(String sentence) {
+		this.sentence = sentence;
+				
 	}
 		
 	
