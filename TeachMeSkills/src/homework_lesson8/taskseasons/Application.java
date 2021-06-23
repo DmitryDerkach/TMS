0f25054ca1c87475cc3a24bc2938dbd764742477
Dmitry <Dmitry@DESKTOP-3KI6QOB>
2021-06-23 17:51:51 +0300

    Everyday routine

--- TeachMeSkills/src/homework_lesson8/taskseasons/Application.java
+++ TeachMeSkills/src/homework_lesson8/taskseasons/Application.java
@@ -3,8 +3,14 @@ package homework_lesson8.taskseasons;
 public class Application {
 public static void main(String[] args) {
 	Seasons s0 = Seasons.SPRING;
-	s0.getInfo();
-	
+	Seasons s1 = Seasons.SUMMER;
+	Seasons s2 = Seasons.AUTUMN;
+	Seasons s3 = Seasons.WINTER;
+		Seasons [] s = {s0,s1,s2,s3};
+			for (Seasons sup: s) {
+				sup.getInfo();
+				System.out.println(sup.getDescription());
+			}
+	Seasons.iLove(s0);
 }
-
 }
--- TeachMeSkills/src/homework_lesson8/taskseasons/Seasons.java
+++ TeachMeSkills/src/homework_lesson8/taskseasons/Seasons.java
@@ -3,32 +3,48 @@ package homework_lesson8.taskseasons;
 import java.util.Iterator;
 
 public enum Seasons {
-	WINTER("Зима - Холодно и идет снег"), SPRING("Весна - Природа оживает"), SUMMER("Лето - Ебучий зной"), AUTUMN("Осень - Ебучая грязь");
+	WINTER("Зима - Холодно и идет снег", -30), 
+	SPRING("Весна - Природа оживает", +25){
+		public String getDescription() {
+			return "Теплое время года";
+		}
+	}, 
+	SUMMER("Лето - Ебучий зной", +35){
+		public String getDescription() {
+			return "Теплое время года";
+		}
+	}, 
+	AUTUMN("Осень - Ебучая грязь", -5);
 	private String forinformation; 
-	Seasons(String string) {
+	private int temp;
+	Seasons(String string, int temp) {
 		forinformation = string;
+		this.temp = temp;
 	}
 public void getInfo() {
-	System.out.println(forinformation);
+	System.out.println(forinformation + ". Средняя температура: " + temp);
 }
-public void iLove (Seasons s) {
+public static void iLove (Seasons s) {
 			switch (s) {
-			case(WINTER):{
+			case WINTER:{
 				System.out.println("Я люблю зиму");
 				break;
 			}
-			case(SPRING):{
+			case SPRING:{
 				System.out.println("Я люблю весну");
 				break;
 			}
-			case(SUMMER):{
+			case SUMMER:{
 				System.out.println("Я люблю лето");
 				break;	
 			}
-			case(AUTUMN):{
+			case AUTUMN:{
 				System.out.println("Я люблю осень");
 				break;
 			}
 		}
 	}
+public String getDescription() {
+	return "Холодное время года";
+}
 }
