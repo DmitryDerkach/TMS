d9c542cee620313ac8613b508be3f59e0b5ea8d5
Dmitry <Dmitry@DESKTOP-3KI6QOB>
2021-06-27 20:43:33 +0300

    Everyday routine

--- TeachMeSkills/src/homework_lesson9/task4/Appliaction.java
+++ TeachMeSkills/src/homework_lesson9/task4/Appliaction.java
@@ -7,7 +7,79 @@ public class Appliaction {
 System.out.println("Вводите предложения. Предложения раздляйте точкой: ");
 Scanner sc = new Scanner(System.in);
 String line = sc.nextLine();
+/*Нужно посчиатать кол-во точек. По ним определим размерность будущего маасива*/
 char [] arrayofsymbols = line.toCharArray();
+int counter = 0;
+	for (int i = 0; i < arrayofsymbols.length; i++) {/*Кол-во точек равно кол-ву предложений*/
+		if (arrayofsymbols[i] == '.') {
+			counter += 1;
+		}
+	}
+/*Создаем массив строк с кол-вом ячеек равынм кол-ву предложений*/
+String [] arraywithlines = new String [counter];
+/*Теперь в ячейки наешго массива нужно положить строки*/
+int supcounter = 0;
+int supcounter2 = 0;
+for (int i = 0; i < arrayofsymbols.length; i++) {	
+	if (arrayofsymbols[i] == '.') { /*Считываем массив символов, встречаем точку. В первый раз записываем в строку все, что от 0 и до точки*/
+			 StringBuilder strb = new StringBuilder();
+			 if (supcounter == 0) {
+				for (int z = 0; z < i; z++) { /*i содежит индекс точки, которую не учитываем, поэтому z < i*/
+						strb.append(arrayofsymbols[z]);	
+				}
+		arraywithlines [supcounter]	= strb.toString();	
+		supcounter +=1;/*Вручную прописываем иттерацию нашего массива строк*/
+		supcounter2 = i; /*Это число понадобится для грамотного оформления массива стрингов, т.к. второе слово мы будем считывать уже не с 0*/
+			} else {
+				for (int z = i-1; z > supcounter2; z--) {
+					strb.append(arrayofsymbols[z]);	
+			}
+	supcounter2 = i;			
+	strb.reverse();
+	arraywithlines [supcounter]	= strb.toString();	
+	supcounter +=1;
+			}				 
+		}		 			 
+	}
+/*Так, массив строк мы создали. Идем оформлять TextFornater*/
+/*Спустя 10 тысяч лет - оформил*/
+/*Проверочка*/
+	for (int i = 0; i < arraywithlines.length; i++) {
+		TextFormater.getLine(arraywithlines[i]);
+	}
+
+
+
+
+
+
+
+
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Задача - разбить нашу писанину в предложения*/
+
+//char [] arrayofsymbols = line.toCharArray();/*Раскаладываем нашу строку в символы и заносим в массив*/
+//for (int i = 0; i < arrayofsymbols.length; i++) {
+//	if (arrayofsymbols[i] == '.') { /*Встретили точку. Работаем с символами, которые у нас стояли до этой точки*/
+//		
+//		
+//		
+//	}
+//}
 		
 		
 		
@@ -15,5 +87,4 @@ char [] arrayofsymbols = line.toCharArray();
 		
 		
 
-	}
-}
+
--- TeachMeSkills/src/homework_lesson9/task4/TextFormater.java
+++ TeachMeSkills/src/homework_lesson9/task4/TextFormater.java
@@ -1,15 +1,44 @@
 package homework_lesson9.task4;
 
 public class TextFormater {
-	public int getLine(String line) {
-		return 0;
+	public static void getLine(String line) {
+		char [] sup = line.toCharArray();
+			if ((sup.length >= 3) && (sup.length <= 5)){
+				System.out.println("Слово " + line + " удовлетворяет условию");
+			} else {
+					palindromCheck(line);
+					if (palindromCheck(line) == true) {
+						System.out.println("Слово " + line + " является палиндромом");
+					} else {
+						System.out.println("Слово " + line + "  не удовлетворяет условию");
+					}
+			}
 	}
-	public void palindromCheck() {
-		boolean supvalue = true;
-		if() {
-			supvalue = true;
+	private static boolean  palindromCheck(String line) {
+		boolean sup = false;
+		int counter = 0;
+		char [][] suparr = new char [1][];   
+		suparr[0] = line.toCharArray();		
+		int arrint = suparr[0].length;	
+		for(int z = 0; z < arrint; z++) {
+		if (suparr[0][z] != suparr[0][arrint - 1 - z]) {
+			if ((Character.toUpperCase(suparr[0][z]) == suparr[0][arrint - 1 - z]) || (Character.toLowerCase(suparr[0][z]) == suparr[0][arrint - 1 - z])) {
+				counter+=1;
+				if (counter == arrint) {
+					sup = true;
+				}
+			} else {
+				sup = false;
+			}	
 		} else {
-			supvalue = false;
+			counter+=1;
+			if (counter == arrint) {
+				sup = true;
+			} 
 		}
 	}
+return sup;	
 }
+
+}
+	
