f3c803941e4e58958ed7bd44171389cf5af7dd59
Dmitry <Dmitry@DESKTOP-3KI6QOB>
2021-07-04 18:58:26 +0300

    Fix

---
+++ TeachMeSkills/src/homework_lesson11/task2/Application.java
@@ -0,0 +1,53 @@
+package homework_lesson11.task2;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Application  {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+			FileReader fr1 = new FileReader("C:\\repositories\\TMS\\TeachMeSkills\\src\\homework_lesson11\\task2\\task2fileN1");
+			FileWriter fw1 = new FileWriter("C:\\repositories\\TMS\\TeachMeSkills\\src\\homework_lesson11\\task2\\task2fileN2");
+			/*Предложения разделяются точкой, восклицательным или вопросительным знаком*/
+			int a = 0;/*Вспомогатлеьный счетчик*/
+			int counter = 0; /*Счетчик, который крутит длинну массива*/
+			String [] arrayofstrings = null; /*Массив, который мы собираемся заполнять*/
+			try {
+				while ((a = fr1.read()) != -1)/*Крутим наш текст*/ {
+					fw1.write(a); /*Костыль. Я не знаю, как мне полсе того, как я прочел все символы, вернуться в начало, поэтому, на
+					данном этапе, сразу записываю читаемые символы в новый поток вывода*/
+					System.out.print((char)a);
+					if (((char)a == '!') || ((char)a == '?') || ((char)a == '.')) {
+						/*Определяем длинну нашего массива строк, в ячейках которого будет хранится строки*/
+						counter +=1;
+						arrayofstrings = new String [counter];
+						/*После того, как мы определились с длинной массива, будем ложить наши строки в ячейки массива*/
+					}
+				}
+			} catch (IOException e1) {
+				System.out.println("IOE here");
+			}
+			/*Заполняем наш массив*/
+			String s = "";
+			a = 0;/*Сбрасываем счетчик на а*/
+			fr1 = new FileReader("C:\\repositories\\TMS\\TeachMeSkills\\src\\homework_lesson11\\task2\\task2fileN2");/*будем читать с 
+			массива, в который мы записались*/
+			for (int i = 0; i < counter; i++) {
+				try {
+					while ((a = fr1.read()) != -1) {/*Забавно. После того, как мы полностью прошлись по
+					тексту, мы не возвращаемся в начало, а просто триггерим -1*/
+						if (((char)a != '!') && ((char)a != '?') && ((char)a != '.')) {
+						s = s.concat(String.valueOf((char)a));
+						System.out.print(s);
+						} else {
+						arrayofstrings[i] = s;
+						System.out.println(arrayofstrings[i]);
+						}
+						
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+	}
+}	
