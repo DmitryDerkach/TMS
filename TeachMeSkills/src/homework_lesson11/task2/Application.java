0145ac73d9f9b2111d0a74b19686981bb7670abe
Dmitry <Dmitry@DESKTOP-3KI6QOB>
2021-07-04 20:28:06 +0300

    Fix

--- TeachMeSkills/src/homework_lesson11/task2/Application.java
+++ TeachMeSkills/src/homework_lesson11/task2/Application.java
@@ -6,8 +6,11 @@ import java.io.IOException;
 
 public class Application  {
 	public static void main(String[] args) {
-			try{FileReader fr1 = new FileReader("C:\\repositories\\TMS\\TeachMeSkills\\src\\homework_lesson11\\task2\\task2fileN1");
-			FileWriter fw1 = new FileWriter("C:\\repositories\\TMS\\TeachMeSkills\\src\\homework_lesson11\\task2\\task2fileN2");
+			FileReader fr1 = null;
+			FileWriter fw1 = null;
+			try { 
+			fr1 = new FileReader("C:\\repositories\\TMS\\TeachMeSkills\\src\\homework_lesson11\\task2\\task2fileN1");
+			fw1 = new FileWriter("C:\\repositories\\TMS\\TeachMeSkills\\src\\homework_lesson11\\task2\\task2fileN2");
 			/*Предложения разделяются точкой, восклицательным или вопросительным знаком*/
 			int a = 0;/*Вспомогательный счетчик*/
 			int counter = 0; /*Счетчик, который крутит длинну массива*/
@@ -23,28 +26,39 @@ public class Application  {
 					}
 				}
 			/*Заполняем наш массив*/
-//			String s = "";
-//			a = 0;/*Сбрасываем счетчик на а*/
-//			fr1 = new FileReader("C:\\repositories\\TMS\\TeachMeSkills\\src\\homework_lesson11\\task2\\task2fileN2");/*будем читать с 
-//			массива, в который мы записались*/
-//			for (int i = 0; i < counter; i++) {
-//				try {
-//					while ((a = fr1.read()) != -1) {
-//						if (((char)a != '!') && ((char)a != '?') && ((char)a != '.')) {
-//						s = s.concat(String.valueOf((char)a));
-//						System.out.print(s);
-//						} else {
-//						arrayofstrings[i] = s;
-//						System.out.println(arrayofstrings[i]);
-//						}
-//						
-//					}
-//				} catch (IOException e) {
-//					e.printStackTrace();
-//				}
-//			}
+			String s = "";
+			a = 0;
+			for (int i = 0; i < counter; i++) {
+				try {
+					while ((a = fr1.read()) != -1) { /*Выбивает -1*/
+						if (((char)a != '!') && ((char)a != '?') && ((char)a != '.')) {
+						s = s.concat(String.valueOf((char)a));
+						System.out.print(s);
+						} else {
+						arrayofstrings[i] = s;
+						System.out.println(arrayofstrings[i]);
+						}
+						
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 			} catch (Exception e) {
 				e.printStackTrace();
+			} finally {
+				try {
+					fr1.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				try {
+					fw1.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 	}
 }	
