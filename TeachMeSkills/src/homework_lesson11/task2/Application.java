c4fced42b625c07f398d3a279bd17568e4ef8fec
Dmitry <Dmitry@DESKTOP-3KI6QOB>
2021-07-06 15:02:10 +0300

    ...

--- TeachMeSkills/src/homework_lesson11/task2/Application.java
+++ TeachMeSkills/src/homework_lesson11/task2/Application.java
@@ -1,23 +1,18 @@
 package homework_lesson11.task2;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 
 public class Application  {
 	public static void main(String[] args) {
 			FileReader fr1 = null;
-			FileWriter fw1 = null;
 			try { 
 			fr1 = new FileReader("C:\\repositories\\TMS\\TeachMeSkills\\src\\homework_lesson11\\task2\\task2fileN1");
-			fw1 = new FileWriter("C:\\repositories\\TMS\\TeachMeSkills\\src\\homework_lesson11\\task2\\task2fileN2");
 			/*Предложения разделяются точкой, восклицательным или вопросительным знаком*/
 			int a = 0;/*Вспомогательный счетчик*/
 			int counter = 0; /*Счетчик, который крутит длинну массива*/
 			String [] arrayofstrings = null; /*Массив, который мы собираемся заполнять*/
 			System.out.println("Исходный текст:"); 
 				while ((a = fr1.read()) != -1)/*Крутим наш текст*/ {
-					fw1.write(a); 
 					System.out.print((char)a);
 					if (((char)a == '!') || ((char)a == '?') || ((char)a == '.')) {
 						/*Определяем длинну нашего массива строк, в ячейках которого будет хранится строки*/
@@ -58,12 +53,6 @@ public class Application  {
 			} finally {
 				try {
 					fr1.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				try {
-					fw1.close();
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
